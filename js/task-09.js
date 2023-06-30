d85266d0c3f1923815a360bff14c97eedd9eae9f
Vera Lyamzina <lyamzina91@gmail.com>
2023-06-30 19:40:23 +0300

    Правки

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,16 @@
 
-document.addEventListener('DOMContentLoaded', function () {
+
   const button = document.querySelector('.change-color');
   const colorSpan = document.querySelector('.color');
 
-  button.addEventListener('click', function () {
+button.addEventListener('click', handleClick);
+function handleClick() {
    
     document.body.style.backgroundColor = colorSpan.textContent = getRandomHexColor();
-  });
+  };
 
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
       .toString(16)
       .padStart(6, '0')}`;
-  }
-});
+  };
